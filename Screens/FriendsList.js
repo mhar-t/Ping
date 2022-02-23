3fd44b27f7cb9c8484cd6b227a6e913c99f115cc
emilyredm <emilyredm@gmail.com>
2022-02-22 21:34:14 -0800

    Updated Friends page

--- Screens/FriendsList.js
+++ Screens/FriendsList.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 export default function FriendList({}) {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Friends!</Text>
+      <Text>My Friends!</Text>
     </View>
   );
 }
