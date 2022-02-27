adfd6a6ef35c5959a6cf907fb84c0e73bfb718f1
“mhar-t” <mhartenorio10@gmail.com>
2022-02-27 01:51:52 -0800

    garden of goals home

--- Themes/Colors.js
+++ Themes/Colors.js
@@ -1,6 +1,12 @@
 const colors = {
     black: "#292929",
     darkpink: "#EC908F",
+    darkgreen: '#63926C',
+    brown: '#50493B',
+    white: '#FFFFFF',
+    gray: '#A4A29F',
+    purple: '#27022B',
+    cream: '#F0E6D7',
   };
   
   export default colors;
