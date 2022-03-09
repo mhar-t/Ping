990ed76dfd821764003de03bf51936211802d8d2
ada-zhou <zhouc@stanford.edu>
2022-03-09 13:21:39 -0800

    onboardin

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { AsyncStorage, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
@@ -8,34 +8,55 @@ import FriendList from './Screens/FriendsList';
 import Goals from './Screens/Goals';
 import Colors from './Themes/Colors';
 
+
+import { createStackNavigator } from '@react-navigation/stack';
+import OnboardingScreens from './Screens/Onboarding';
+
+const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
+
+
 export default function App() {
-  return (
+
+    function BottomTabs() {
+        return (
+            <Tab.Navigator screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, color }) => {
+                let iconName;
+                if (route.name === 'Home') {
+                  iconName = 'home'
+                } else if (route.name === 'Friends') {
+                  iconName = 'people-circle-outline'
+                } else if (route.name === 'Garden of Goals') {
+                  iconName="leaf"
+                } else if (route.name === 'Profile') {
+                  iconName="person"
+                }
+                return <Ionicons name={iconName} size={32} color={color} />;
+              },
+              tabBarActiveTintColor: Colors.darkpink,
+              tabBarInactiveTintColor: Colors.black,
+              headerShown: false,
+              tabBarShowLabel: false,
+            })}>
+            <Tab.Screen name="Home" component={HomeStack} options={{headerShown: false}}/>
+            <Tab.Screen name="Friends" component={FriendList} options={{headerShown: false}}/>
+            <Tab.Screen name="Garden of Goals" component={Goals} options={{headerShown: false}}/>
+          </Tab.Navigator> 
+        )
+    }
+
+  return (  
     <NavigationContainer>
-      <Tab.Navigator screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color }) => {
-            let iconName;
-            if (route.name === 'Home') {
-              iconName = 'home'
-            } else if (route.name === 'Friends') {
-              iconName = 'people-circle-outline'
-            } else if (route.name === 'Garden of Goals') {
-              iconName="leaf"
-            } else if (route.name === 'Profile') {
-              iconName="person"
-            }
-            return <Ionicons name={iconName} size={32} color={color} />;
-          },
-          tabBarActiveTintColor: Colors.darkpink,
-          tabBarInactiveTintColor: Colors.black,
-          headerShown: false,
-          tabBarShowLabel: false,
-        })}>
-        <Tab.Screen name="Home" component={HomeStack} options={{headerShown: false}}/>
-        <Tab.Screen name="Friends" component={FriendList} options={{headerShown: false}}/>
-        <Tab.Screen name="Garden of Goals" component={Goals} options={{headerShown: false}}/>
-      </Tab.Navigator>
+        <Stack.Navigator>
+            <Stack.Screen
+            name="Onboarding"
+            component={OnboardingScreens}
+            options={{headerShown: false}}
+            />
+            <Stack.Screen name="Home" component={BottomTabs} options={{headerShown: false}}/>
+        </Stack.Navigator>
     </NavigationContainer>
   );
 }
---
+++ Screens/Onboarding.js
@@ -0,0 +1,42 @@
+import { Image } from 'react-native';
+import React from 'react';
+import colors from '../Themes/Colors';
+import Onboarding from 'react-native-onboarding-swiper';
+import { useNavigation } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
+
+export default function OnboardingScreens({navigation}) {
+    return ( <
+            Onboarding 
+                onDone = {() => navigation.navigate("Home")}
+                onSkip = {() => navigation.navigate("Home")}
+                titleStyles={{fontSize: 24, fontWeight: 'bold',}}
+                subTitleStyles={{fontSize: 18, marginLeft: 10, marginRight: 10}}
+            pages = { 
+                [{
+                        backgroundColor: colors.background,
+                        image: < Image source = { require('../assets/onboarding/tokens.png') }
+                        />,
+                        title: 'Exchange Tokens',
+                        subtitle: 'Make inside jokes or share what\'s going on in your life with your loved ones',
+                    },
+                    {
+                        backgroundColor: colors.background,
+                        image: < Image source = { require('../assets/onboarding/moments.png') }
+                        />,
+                        title: 'Create Moments',
+                        subtitle: 'Take a moment to think about your loved one. We\'ll let them know you thought of them.',
+                    },
+                    {
+                        backgroundColor: colors.background,
+                        image: < Image source = { require('../assets/onboarding/goals.png') }
+                        />,
+                        title: 'Maintain Connection Goals',
+                        subtitle: "Don't worry, we won\'t treat connecting with your loved ones a chore so you won't see metrics or charts. Instead we'll show you generally how well you've been 'watering' your goals.",
+                    },
+                ]
+            }
+            />
+        );        
+    
+}
