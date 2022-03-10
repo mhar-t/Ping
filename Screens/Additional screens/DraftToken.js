ea07b02a74b5517540be60ad011d36687567eead
emilyredm <emilyredm@gmail.com>
2022-03-09 18:29:26 -0800

    updated input field and added static prompt to draft token page

--- Screens/Additional screens/DraftToken.js
+++ Screens/Additional screens/DraftToken.js
@@ -4,6 +4,8 @@ import { View, Text, StyleSheet, Pressable, Image, TextInput} from 'react-native
 import { useNavigation } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
 import { SafeAreaView } from 'react-native-safe-area-context';
+//import {Prompt} from '../../Components/Prompt'; 
+
 
 
 
@@ -11,7 +13,6 @@ import colors from '../../Themes/Colors';
 import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 
-// TODO CLEAN UP LAYOUT 
 
 export default function DraftToken({}) {
     const navigation = useNavigation();
@@ -36,13 +37,21 @@ export default function DraftToken({}) {
                 <View style={styles.lastToken}>
                     
                 </View>
-                <View style={styles.prompt}>
-                    
+                <View style={styles.promptContainer}>
+                    <View style={styles.prompt}>
+                        <Image source={require('../../assets/icons/generate.png')} style={{width: 40, height: 40}}/>
+                        <View style={styles.promptText}>
+                            <Text style={{fontSize: 15}}>Not sure what to send? Here's an idea:</Text>
+                            <Text style={{fontWeight: 'bold', fontSize: 14}}>What is your favorite memory with this friend?</Text>
+                        </View> 
+                    </View>
                 </View>
                 <View style={styles.input}>
                     <View style={styles.inputBox}>
                         <TextInput
                         placeholder="Type up to 5 emojis..."
+                        maxLength={5}
+                        style={styles.text}
                         />
                     </View> 
                 </View>
@@ -103,24 +112,53 @@ const styles = StyleSheet.create({
         flex: 2,
         //backgroundColor: 'blue'
     },
-    prompt: {
+    promptContainer: {
         flex: 3,
+        paddingTop: 25
         //backgroundColor: 'red'
 
     }, 
+    prompt:{
+        marginLeft: '4%',
+        marginRight: '4%',
+        height: '50%', 
+        justifyContent: 'center', 
+        alignItems: 'center',
+        flexDirection: 'row',
+        borderRadius: 15,
+        backgroundColor: 'white', 
+        
+        shadowColor: "#292929",
+        shadowOpacity: 0.2,
+        shadowRadius: 5,
+        shadowOffset: { width: -1, height: 5 },
+    }, 
+    promptText:{
+        flexDirection: 'column',
+        justifyContent: 'center', 
+        alignItems: 'center',
+        paddingLeft: 10 
+    },
     input:{
         flex: 4,
         backgroundColor: colors.purewhite,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center', 
+
+        shadowColor: "#292929",
+        shadowOpacity: 0.2,
+        shadowRadius: 5,
+        shadowOffset: { width: -1, height: 5 },
     }, 
     inputBox:{
         flex: 4,
         backgroundColor: colors.purewhite,
         alignItems: 'center',
         justifyContent: 'center'
-
     }, 
+    text:{
+        fontSize: 42
+    },
     send:{
         flex: 2,
         //backgroundColor: 'yellow'
